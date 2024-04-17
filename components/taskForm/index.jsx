c1984732e810d2e2f021755bbb2face3ba939d1d
eep <coolicer@foxmail.com>
2024-04-17 17:51:39 +0800

    feat: format

--- components/taskForm/index.jsx
+++ components/taskForm/index.jsx
@@ -1,23 +1,33 @@
 'use client';
-import { useRef } from "react";
-import { createTask } from "../../utils/actions";
+import { useRef } from 'react';
+import { createTask } from '../../utils/actions';
 
 const TaskForm = () => {
-  const ref = useRef(null)
+  const ref = useRef(null);
 
   return (
-    <form ref={ref} action={
-      async (formData) => {
-        await createTask(formData)
-        ref.current?.reset()
-      }
-    }>
+    <form
+      ref={ref}
+      action={async (formData) => {
+        await createTask(formData);
+        ref.current?.reset();
+      }}>
       <div className='join w-full'>
-        <input type="text" className='input input-bordered join-item w-full' placeholder='type hrere' name="content" required />
-        <button type='submit' className='btn btn-primary join-item'>creat task</button>
+        <input
+          type='text'
+          className='input input-bordered join-item w-full'
+          placeholder='type hrere'
+          name='content'
+          required
+        />
+        <button
+          type='submit'
+          className='btn btn-primary join-item'>
+          creat task
+        </button>
       </div>
     </form>
-  )
-}
+  );
+};
 
-export default TaskForm
+export default TaskForm;
