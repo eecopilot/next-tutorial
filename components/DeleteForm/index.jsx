5dcb1cec2c20f5542e4cd26e425d0476b3848763
eep <coolicer@foxmail.com>
2024-04-22 15:53:28 +0800

    feat: delete button

--- components/DeleteForm/index.jsx
+++ components/DeleteForm/index.jsx
@@ -1,12 +1,42 @@
-import { deleteTask } from "@/utils/actions"
+'use client';
+// import { useFormStatus } from "react-dom"
+import { deleteTask } from '@/utils/actions';
 
-const DeleteForm = ({id}) => {
+const SubmitButton = () => {
+  // const { pending } = useFormStatus();
+  return (
+    <button
+      type='submit'
+      className='btn btn-error btn-xs'>
+      Delete
+    </button>
+  );
+};
+
+const DeleteForm = ({ id }) => {
   return (
     <form action={deleteTask}>
-      <input type="hidden"  name="id" value={id}/>
-      <button type='submit' className='btn btn-error btn-xs'>Delete</button>
+      <input
+        type='hidden'
+        name='id'
+        value={id}
+      />
+      <SubmitButton />
     </form>
-  )
-}
+  );
+};
 
-export default DeleteForm
+export default DeleteForm;
+
+// import { deleteTask } from "@/utils/actions"
+
+// const DeleteForm = ({id}) => {
+//   return (
+//     <form action={deleteTask}>
+//       <input type="hidden"  name="id" value={id}/>
+//       <button type='submit' className='btn btn-error btn-xs'>Delete</button>
+//     </form>
+//   )
+// }
+
+// export default DeleteForm
